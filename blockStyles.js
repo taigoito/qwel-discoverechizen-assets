541eb4600e0c7d5be3a4b8bea5ebef070d72d7e0
Taigo Ito <webmaster@qwel.design>
2022-02-05 16:36:06 +0900

    Revert "Register a custom style for block quotes"
    This reverts commit 5d145d8da7bc9c633086ac248d48ad832dd2005d.

--- blockStyles.js
+++ blockStyles.js
@@ -5,12 +5,6 @@
  * @package Qwel-Assets
  */
 
-((wp) => {
+(() => {
   console.log('Hello block styles!');
-
-  wp.blocks.registerBlockStyle('core/quote', {
-    name: 'qwel-custom',
-    label: 'Qwel Custom'
-  });
-
-})(wp);
+})();
