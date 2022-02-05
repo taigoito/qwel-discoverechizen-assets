e153640b6b09eb43f13467a0368f3a284acda14e
Taigo Ito <webmaster@qwel.design>
2022-02-05 22:29:29 +0900

    Implement the Drawer Menu

---
+++ js/init.js
@@ -0,0 +1,18 @@
+/**
+ * Init
+ * Author: Taigo Ito (https://qwel.design/)
+ * Location: Fukui, Japan
+ * @package Qwel-Assets
+ */
+
+// Drawer Menu
+import DrawerMenu from "./_drawerMenu.js";
+new DrawerMenu();
+
+// Scrolling
+import Scrolling from './_scrolling.js';
+new Scrolling();
+
+// Evil Icons
+import EvilIcons from './_evilIcons.js';
+new EvilIcons();
