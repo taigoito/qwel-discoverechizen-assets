5d145d8da7bc9c633086ac248d48ad832dd2005d
Taigo Ito <webmaster@qwel.design>
2022-02-05 15:11:22 +0900

    Register a custom style for block quotes

--- blockStyles.js
+++ blockStyles.js
@@ -5,6 +5,12 @@
  * @package Qwel-Assets
  */
 
-(() => {
+((wp) => {
   console.log('Hello block styles!');
-})();
+
+  wp.blocks.registerBlockStyle('core/quote', {
+    name: 'qwel-custom',
+    label: 'Qwel Custom'
+  });
+
+})(wp);
